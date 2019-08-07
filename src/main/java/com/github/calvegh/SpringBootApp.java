4635d13b67ad895e464e112d0485117be9587b35
Cavle <plabdnet01@gmail.com>
2019-08-08 01:41:15 +0300

    heroku deploy

--- src/main/java/com/github/calvegh/SpringBootApp.java
+++
@@ -1,9 +0,0 @@
-package com.github.calvegh;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class SpringBootApp extends SpringBootServletInitializer {
-}
-
